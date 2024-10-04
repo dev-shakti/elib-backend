054b7293854756ac588fd66c2ab61cb07b9ffc5e
shakti sumant das <shakti.20k@gmail.com>
2024-10-04 15:14:41 +0530

    add createUser

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 import express from "express";
 import globalErrorHandlers from "./middlewares/globalErrorHandlers";
+import userRouter from "./user/userRouter";
 const app=express();
 
 //Routes
 app.get("/", (req,res,next) => {
     res.send("Welcome to elib books")
 })
+
+app.use("/api/users", userRouter)
 
 // Global error handlers
 app.use(globalErrorHandlers)
