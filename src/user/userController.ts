bd90bd4dec773b718cf3365209e25c50028f654e
shakti sumant das <shakti.20k@gmail.com>
2024-10-06 23:45:36 +0530

    add validation and db call in createuser endpoint

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -1,6 +1,21 @@
 import { Request, Response, NextFunction } from "express";
+import createHttpError from "http-errors";
+import { User } from "./userModel";
 
-const createUser = (req: Request, res: Response, next: NextFunction) => {
+const createUser = async(req: Request, res: Response, next: NextFunction) => {
+
+  const {name, email, password}=req.body;
+
+  //validation
+  if(!name || !email || !password){
+    next(createHttpError(400, 'All fields are required'));
+  }
+  //db call
+  const newUser=await User.find({email})
+  if(newUser){
+    next(createHttpError(400, 'Email already exists'));
+  }
+  
   res.json({ msg: "User created successfully" });
 };
 
