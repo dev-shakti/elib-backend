68514d2b781a7707d3f899a9a93017a078006d2b
shakti sumant das <shakti.20k@gmail.com>
2024-10-07 15:30:53 +0530

    create new user and add error handling in signup

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -2,23 +2,47 @@ import { Request, Response, NextFunction } from "express";
 import createHttpError from "http-errors";
 import { User } from "./userModel";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { config } from "../config/config";
+
+const createToken= (_id: string) => {
+   // Check if the secret is available
+  if (!config.secret) {
+    throw new Error("JWT secret not found in config");
+}    
+ // Generate the token
+     return jwt.sign({_id},config.secret, {expiresIn:"7d"})
+}
 
 const createUser = async(req: Request, res: Response, next: NextFunction) => {
 
+ try {
   const {name, email, password}=req.body;
 
-  //validation
+  //Validation
   if(!name || !email || !password){
     return next(createHttpError(400, 'All fields are required'));
   }
-  //db call
-  const newUser=await User.findOne({email})
-  if(newUser){
+  //DB call
+  const existingUser=await User.findOne({email})
+  if(existingUser){
     return next(createHttpError(400, 'Email already exists'));
   }
+
+  //Hashed the password for security
   const hashedPassword=await bcrypt.hash(password,10)
+
+  //Create user in the database
   const user=await User.create({name,email, password:hashedPassword})
-  res.json({ });
+
+   // Generate a JWT token
+  const token=createToken(user._id)
+
+  // Respond with the user details and token
+  res.status(201).json({ email, token});
+ } catch (error) {
+      next(error)
+ }
 };
 
 export default createUser;
