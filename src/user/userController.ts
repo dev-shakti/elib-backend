2268dfa31307c8d66324cb9fc7877e8fd6c36e6d
shakti sumant das <shakti.20k@gmail.com>
2024-10-07 16:22:08 +0530

    add logic to login user

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -5,48 +5,72 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import { config } from "../config/config";
 
-const createToken= (_id: string) => {
-   // Check if the secret is available
+const createToken = (_id: string) => {
+  // Check if the secret is available
   if (!config.secret) {
     throw new Error("JWT secret not found in config");
-}    
- // Generate the token
-     return jwt.sign({_id},config.secret, {expiresIn:"7d"})
-}
-
-const createUser = async(req: Request, res: Response, next: NextFunction) => {
-
- try {
-  const {name, email, password}=req.body;
-
-  //Validation
-  if(!name || !email || !password){
-    return next(createHttpError(400, 'All fields are required'));
   }
-  //DB call
-  const existingUser=await User.findOne({email})
-  if(existingUser){
-    return next(createHttpError(400, 'Email already exists'));
-  }
-
-  //Hashed the password for security
-  const hashedPassword=await bcrypt.hash(password,10)
-
-  //Create user in the database
-  const user=await User.create({name,email, password:hashedPassword})
-
-   // Generate a JWT token
-  const token=createToken(user._id)
-
-  // Respond with the user details and token
-  res.status(201).json({ email, token});
- } catch (error) {
-      next(error)
- }
+  // Generate the token
+  return jwt.sign({ _id }, config.secret, { expiresIn: "7d" });
 };
 
-const loginUser = async(req: Request, res: Response, next: NextFunction) => {
-  res.json({})
-}
+const createUser = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { name, email, password } = req.body;
 
-export  {createUser,loginUser};
+    //Validation
+    if (!name || !email || !password) {
+      return next(createHttpError(400, "All fields are required"));
+    }
+    //DB call
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return next(createHttpError(400, "Email already exists"));
+    }
+
+    //Hashed the password for security
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    //Create user in the database
+    const user = await User.create({ name, email, password: hashedPassword });
+
+    // Generate a JWT token
+    const token = createToken(user._id);
+
+    // Respond with the user details and token
+    res.status(201).json({ email, token });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const loginUser = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { email, password } = req.body;
+
+    //Validation
+    if (!email || !password) {
+      return next(createHttpError(400, "All fields are required"));
+    }
+
+    // Find user by email
+    const user = await User.findOne({ email }).select("+password");
+    if (!user) {
+      return next(createHttpError(404, "User not found"));
+    }
+
+    // Compare password
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return next(createHttpError(400, "Invalid credentials"));
+    }
+    
+    //Create token
+    const token = createToken(user._id);
+    res.status(201).json({ email: user.email, token });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export { createUser, loginUser };
